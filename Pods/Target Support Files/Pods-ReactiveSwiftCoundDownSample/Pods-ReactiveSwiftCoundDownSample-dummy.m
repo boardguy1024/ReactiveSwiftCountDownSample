881893a0cf73ecbba990493047980f2a803ec242
park kyung suk <boardguy1024@gmail.com>
2019-04-23 22:57:32 +0900

    first commit

---
+++ Pods/Target Support Files/Pods-ReactiveSwiftCoundDownSample/Pods-ReactiveSwiftCoundDownSample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactiveSwiftCoundDownSample : NSObject
+@end
+@implementation PodsDummy_Pods_ReactiveSwiftCoundDownSample
+@end
---
+++ ReactiveSwiftCoundDownSample/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  ReactiveSwiftCoundDownSample
+//
+//  Created by park kyung suk on 2019/04/23.
+//  Copyright © 2019 park kyung suk. All rights reserved.
+//
+
+import UIKit
+import ReactiveSwift
+import Result
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var countdownLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        startCountdown(sec: 3, targetLabel: self.countdownLabel)
+            .startWithCompleted {
+                print("Finished Countdown")
+                //カウントダウンが完了後処理したいコードを書く
+            }
+        
+    }
+    
+    
+    func startCountdown(sec: Double, targetLabel: UILabel) -> SignalProducer<Void, NoError> {
+        targetLabel.isHidden = true
+        
+        return SignalProducer<Void, NoError> { (observer, lifetime) in
+            
+            self.createCountdownSignalProducer(sec: sec)
+                .skipRepeats()
+                .observe(on: UIScheduler())
+                .startWithSignal { (signal, disposable) in
+                    lifetime += targetLabel.reac
+                }
+            observer.sendCompleted()
+        }
+    }
+    
+    private func createCountdownSignalProducer(sec: Double) -> SignalProducer<Int, NoError> {
+        let endTargetDate = Date().addingTimeInterval(sec)
+        
+        return SignalProducer.timer(interval: .milliseconds(50), on: QueueScheduler()).map { timeDate in
+            // 発火したタイミングより指定した秒までに .millisecond(50)ごとにシグナルを送信
+            endTargetDate.timeIntervalSince(timeDate)
+            } // .take { } は 条件が成立すると 自動的に.completedされる
+            .take { timeInterval in timeInterval > 0 }
+            // TimeIntervalを Intに整形して送信
+            .map { timeInterval in Int(timeInterval) + 1}
+    }
+
+
+}
+
